e481ea2b0bf651ec31ba4ecd212cd9c86a91b7db
Gwen <gwenaelle.batta@studetn.hepl.be>
2022-02-25 13:14:39 +0100

    draw triangle end, mastercard end and home center

--- src/js/main.ts
+++ src/js/main.ts
@@ -21,86 +21,117 @@ function drawSwissFlag(){
 drawSwissFlag();
 
 //Home
+function home(){
+
 const fondHome = document.getElementById('home');
 let ctxHome = fondHome.getContext('2d');
 ctxHome.fillStyle='white';
 ctxHome.fillRect(0,0,fondHome.width,fondHome.height);
 // Toit
-ctxHome.moveTo(40, 80);
-ctxHome.lineTo(80, 40);
-ctxHome.lineTo(120, 80);
+ctxHome.moveTo((fondHome.width/2)-40, (fondHome.height/2)+10);
+ctxHome.lineTo(fondHome.width/2,(fondHome.height/2)-40;
+ctxHome.lineTo((fondHome.width/2)+40, (fondHome.height/2)+10);
 // Murs
-ctxHome.moveTo(60, 80);
-ctxHome.lineTo(60, 120);
-ctxHome.lineTo(100, 120);
-ctxHome.lineTo(100, 80);
-ctxHome.rect(75,100,10,20);
+ctxHome.moveTo((fondHome.width/2)-20, (fondHome.height/2)+10);
+ctxHome.lineTo((fondHome.width/2)-20, (fondHome.height/2)+50);
+ctxHome.lineTo((fondHome.width/2)+20, (fondHome.height/2)+50);
+ctxHome.lineTo((fondHome.width/2)+20, (fondHome.height/2)+10);
+ctxHome.rect((fondHome.width/2)-5, (fondHome.height/2)+30,10,20);
 ctxHome.strokeStyle = "red";
 ctxHome.lineWidth = 3;
 ctxHome.stroke();
 // Palmier
 ctxHome.beginPath();
 ctxHome.strokeStyle = "LimeGreen";
-ctxHome.moveTo(20,120);
-ctxHome.arcTo(20,30,160,120,20);
+ctxHome.moveTo((fondHome.width/2)-60,(fondHome.height/2)+60);
+ctxHome.arcTo((fondHome.width/2)-60,(fondHome.height/2)-40,(fondHome.width/2)-20,160,20);
 ctxHome.stroke();
 // Soleil
 ctxHome.fillStyle = "yellow";
 ctxHome.strokeStyle = "orange";
 ctxHome.beginPath();
-ctxHome.arc(150,40,30,0,Math.PI*2,true);
+ctxHome.arc((fondHome.width/2)+60,(fondHome.height/2)-40,30,0,Math.PI*2,true);
 ctxHome.fill();
 ctxHome.stroke();
 // Vaguelettes
 ctxHome.strokeStyle = "turquoise";
 ctxHome.beginPath();
-ctxHome.moveTo(20, 150);
-ctxHome.bezierCurveTo(80,130,80,180,140,150);
-ctxHome.moveTo(20, 170);
-ctxHome.bezierCurveTo(80,150,80,200,140,170);
+ctxHome.moveTo((fondHome.width/2)+60, (fondHome.height/2)+80);
+ctxHome.bezierCurveTo((fondHome.width/2)+120,(fondHome.height/2)+60,(fondHome.width/2)+120,(fondHome.height/2)+110,(fondHome.width/2)+200,(fondHome.height/2)+80);
+ctxHome.moveTo((fondHome.width/2)+60, (fondHome.height/2)+100);
+ctxHome.bezierCurveTo((fondHome.width/2)+120,(fondHome.height/2)+80,(fondHome.width/2)+120,(fondHome.height/2)+130,(fondHome.width/2)+200,(fondHome.height/2)+100);
 ctxHome.stroke();
 // Sourire
 ctxHome.beginPath();
 ctxHome.strokeStyle = "orange";
-ctxHome.moveTo(130, 40);
-ctxHome.quadraticCurveTo(150,70,170,40);
+ctxHome.moveTo((fondHome.width/2)+40,(fondHome.height/2)-30);
+ctxHome.quadraticCurveTo((fondHome.width/2)+60,(fondHome.height/2)-10,(fondHome.width/2)+80,(fondHome.height/2)-30);
 ctxHome.stroke();
+}
+home();
+
 //Mastercard
-
-const fondMaster = document.getElementById('master-card');
-let ctxMc = fondMaster.getContext('2d');
-ctxMc.fillStyle='black';
-ctxMc.fillRect(0,0,fondMaster.width,fondMaster.height);
+function masterCard(){
+    const fondMaster = document.getElementById('master-card');
+    let ctxMc = fondMaster.getContext('2d');
+    ctxMc.fillStyle='black';
+    ctxMc.fillRect(0,0,fondMaster.width,fondMaster.height);
 //Double cercle
-ctxMc.beginPath();
-ctxMc.fillStyle = "red";
-ctxMc.arc((fondMaster.width*1.5/4),(fondMaster.height/2)-35,(fondMaster.height/3),0,Math.PI*2);
-ctxMc.fillStyle = "yellow";
-ctxMc.arc((fondMaster.width*2.5/4),(fondMaster.height/2)-35,(fondMaster.height/3),0,Math.PI*2);
-ctxMc.closePath();
-ctxMc.fill();
-ctxMc.stroke();
-//Texte
-ctxMc.font = "50px Century";
-ctxMc.fillStyle = "white";
-ctxMc.textAlign = "center";
-ctxMc.fillText("mastercard", fondMaster.width/2, fondMaster.height*7/8);
+    ctxMc.fillStyle = "red";
+    ctxMc.beginPath();
+    ctxMc.arc((fondMaster.width*1.5/4),(fondMaster.height/2)-35,(fondMaster.height/3),0,Math.PI*2);
+    ctxMc.fill();
+    ctxMc.fillStyle = "yellow";
+    ctxMc.beginPath();
+    ctxMc.arc((fondMaster.width*2.5/4),(fondMaster.height/2)-35,(fondMaster.height/3),0,Math.PI*2);
+    ctxMc.fill();
+    //Texte
+    ctxMc.font = "50px Century";
+    ctxMc.fillStyle = "white";
+    ctxMc.textAlign = "center";
+    ctxMc.fillText("mastercard", fondMaster.width/2, fondMaster.height*7/8);
+    //Ellipse
+    ctxMc.fillStyle = "orange";
+    ctxMc.scale(0.5,1);
+    ctxMc.beginPath();
+    ctxMc.arc((fondMaster.width),(fondMaster.height/2)-35,(fondMaster.height/4),0,Math.PI*2);
+    ctxMc.fill();
+
+}
+masterCard();
 
 //Triangle
-
-const fondTriangle = document.getElementById('triangle');
-let ctxTriangle = fondTriangle.getContext('2d');
-ctxTriangle.fillStyle='grey';
-ctxTriangle.fillRect(0,0,fondTriangle.width,fondTriangle.height);
+function triangles(){
+    const fondTriangle = document.getElementById('triangle');
+    let ctxTriangle = fondTriangle.getContext('2d');
+    ctxTriangle.fillStyle='grey';
+    ctxTriangle.fillRect(0,0,fondTriangle.width,fondTriangle.height);
 //Triangle
-ctxTriangle.fillStyle = "black";
-ctxTriangle.beginPath();
-ctxTriangle.moveTo(fondTriangle.width/2,fondTriangle.height/5);
-ctxTriangle.lineTo((fondTriangle.width/2)-100,fondTriangle.height/1.2);
-ctxTriangle.lineTo((fondTriangle.width/2)+100,fondTriangle.height/1.2);
-ctxMc.fillStyle = "white";
-ctxMc.arc((fondTriangle.width/4),(fondTriangle.height/2),(fondTriangle.height/3),0,Math.PI*2);
-ctxTriangle.closePath();
-ctxTriangle.fill();
-ctxTriangle.stroke();
-ctxMc.beginPath();
+    ctxTriangle.fillStyle = "black";
+    ctxTriangle.moveTo(fondTriangle.width/2,fondTriangle.height/5);
+    ctxTriangle.lineTo((fondTriangle.width/2)-100,fondTriangle.height/1.2);
+    ctxTriangle.lineTo((fondTriangle.width/2)+100,fondTriangle.height/1.2);
+    ctxTriangle.fill();
+    ctxTriangle.stroke();
+    ctxTriangle.fillStyle = "white";
+    ctxTriangle.beginPath();
+    ctxTriangle.arc((fondTriangle.width/2),(fondTriangle.height/1.7),(fondTriangle.height/5),0,Math.PI*2);
+    ctxTriangle.fill();
+    ctxTriangle.stroke();
+    ctxTriangle.fillStyle = "black";
+    ctxTriangle.beginPath();
+    ctxTriangle.arc((fondTriangle.width/2)+5,(fondTriangle.height/1.7)-5,(fondTriangle.height/6),0,Math.PI*2);
+    ctxTriangle.fill();
+    ctxTriangle.stroke();
+
+}
+triangles();
+
+function polygonRandom(){
+    const fondPolygon = document.getElementById('shapes');
+    let ctxPolygon = fondPolygon.getContext('2d');
+    ctxPolygon.fillStyle='#101d6b';
+    ctxPolygon.fillRect(0,0,fondPolygon.width,fondPolygon.height);
+}
+
+polygonRandom();
